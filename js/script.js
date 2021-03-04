5d5bd9991027c82df59dbe2815006771ab948b3a
Mario Lozano <mario@outcrop.com>
2021-03-03 17:49:04 -0700

    Testing changes

--- js/script.js
+++ js/script.js
@@ -8,17 +8,15 @@ document.addEventListener("DOMContentLoaded", () => {
                 breakpoint: 1024,
                 settings: {
                     slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true,
-                    arrows: true
+                    slidesToScroll: 1,
+                    infinite: true
                 }
             },
             {
                 breakpoint: 600,
                 settings: {
                     slidesToShow: 2,
-                    slidesToScroll: 2
+                    slidesToScroll: 1
                 }
             },
             {
